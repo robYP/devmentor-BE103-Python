ffe02c5a7542c33fd47622ceeabcf861103f5114
robYP <robchen12@hotmail.com>
2024-07-30 01:16:39 +0800

    create delete api - create delete_post_by_id function in post.py under repository folder

--- repository/post.py
+++ repository/post.py
@@ -17,3 +17,11 @@ def create(db: Session, post: PostCreate):
 
 def get_post_by_id(db: Session, post_id: int):
     return db.query(Post).filter(Post.id == post_id).first()
+
+def delete_post_by_id(db: Session, post_id: int):
+    db_post = db.query(Post).filter(Post.id==post_id).first()
+    if not db_post:
+        return None
+    db.delete(db_post)
+    db.commit()
+    return db_post
