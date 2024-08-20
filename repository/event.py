4418e77b28b9d4dac009222662a608bffb6406c1
robYP <robchen12@hotmail.com>
2024-08-20 11:21:35 +0800

    create search_event_by_id to remove duplicate

--- repository/event.py
+++ repository/event.py
@@ -4,6 +4,14 @@ from database.event import Event
 from database.user import User
 from schema.database.event import EventCreate
 
+
+def search_event_by_id(db:Session, event_id:int):
+    db_event = db.query(Event).filter(Event.id == event_id).first()
+    if not db_event:
+        return None
+    return db_event
+
+
 def get_events(db: Session, skip: int = 0, limit: int = 100):
     return db.query(Event).offset(skip).limit(limit).all()
 
@@ -18,8 +26,8 @@ def create_event(user: User, event: EventCreate, db: Session):
 
 
 def delete_event_by_id(db:Session, event_id:int):
-    db_event = db.query(Event).filter(Event.id == event_id).first()
-    if not db_event:
+    db_event = search_event_by_id(db=db, event_id=event_id)
+    if db_event == None:
         return None
     db.delete(db_event)
     db.commit()
@@ -27,8 +35,8 @@ def delete_event_by_id(db:Session, event_id:int):
 
 
 def update_event_by_id(user: User, event_id: int, event: EventCreate, db: Session):
-    db_event = db.query(Event).filter(Event.id == event_id).first()
-    if not db_event:
+    db_event = search_event_by_id(db=db, event_id=event_id)
+    if db_event == None:
         return None
     db_event.name = event.name 
     db_event.route= event.route
