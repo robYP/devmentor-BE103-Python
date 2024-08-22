e586dba3673fe0d2991198b232868ea8911ccd78
robYP <robchen12@hotmail.com>
2024-08-22 11:50:59 +0800

    remove duplicate code, and create search_event_by_id function in repository > event.py

--- repository/event.py
+++ repository/event.py
@@ -4,8 +4,17 @@ from database.event import Event
 from database.user import User
 from schema.database.event import EventCreate
 
+
+def search_event_by_id(db:Session, event_id:int):
+    db_event = db.query(Event).filter(Event.id == event_id).first()
+    if not db_event:
+        return None
+    return db_event
+
+
 def get_events(db: Session, skip: int = 0, limit: int = 100):
     return db.query(Event).offset(skip).limit(limit).all()
+
 
 def create_event(user: User, event: EventCreate, db: Session):
     db_event = Event(name = event.name, 
@@ -18,25 +27,17 @@ def create_event(user: User, event: EventCreate, db: Session):
 
 
 def delete_event_by_id(db:Session, event_id:int):
-    db_event = db.query(Event).filter(Event.id == event_id).first()
-    if not db_event:
+    db_event = search_event_by_id(db=db, event_id=event_id)
+    if db_event == None:
         return None
     db.delete(db_event)
     db.commit()
     return db_event
 
 
-def delete_post_by_id(db: Session, post_id: int):
-    db_post = db.query(Post).filter(Post.id==post_id).first()
-    if not db_post:
-        return None
-    db.delete(db_post)
-    db.commit()
-    return db_post
-
 def update_event_by_id(user: User, event_id: int, event: EventCreate, db: Session):
-    db_event = db.query(Event).filter(Event.id == event_id).first()
-    if not db_event:
+    db_event = search_event_by_id(db=db, event_id=event_id)
+    if db_event == None:
         return None
     db_event.name = event.name 
     db_event.route= event.route
