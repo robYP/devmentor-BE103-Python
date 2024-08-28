3902c29968ac19c3d9302fed6e0d77fb83b322f3
robYP <robchen12@hotmail.com>
2024-08-28 10:31:22 +0800

    added RecordRepository class in repository > record.py

--- services/event_service.py
+++ services/event_service.py
@@ -3,11 +3,13 @@ import repository.event, repository.record
 from schema.database.event import EventCreate
 
 from repository.event import EventRepository
+from repository.record import RecordRepository
 
 
 class EventService:
     def __init__(self, db: Session):
         self.event_repository = EventRepository(db=db)
+        self.record_repository = RecordRepository(db=db)
         self.db = db
         
 
@@ -18,28 +20,25 @@ class EventService:
 
     def create_event(self, user: dict, event: EventCreate):
         new_event = self.event_repository.create_event(user=user, event=event)
-        repository.record.create_record(user_id=user.id,
-                                        event_id=new_event.id,
-                                        action="Create Event",
-                                        db=self.db)
+        self.record_repository.create_record(user_id = user.id,
+                                       event_id = new_event.id,
+                                       action = "Create Event")
         return new_event
 
 
     def delete_event_by_id(self, user: dict, event_id: int):
         event = self.event_repository.delete_event_by_id(event_id=event_id)
         if event:
-            repository.record.create_record(user_id=user.id,
-                                            event_id=event_id,
-                                            action="Delete Event",
-                                            db=self.db)
+            self.record_repository.create_record(user_id = user.id,
+                                           event_id = event.id,
+                                           action = "Delete Event")
         return event
 
 
     def update_event_by_id(self, user: dict, event_id: int, event: EventCreate):
         updated_event = self.event_repository.update_event_by_id(event_id=event_id, event=event)
         if updated_event:
-            repository.record.create_record(user_id=user.id,
-                                            event_id=updated_event.id,
-                                            action="Update Event",
-                                            db=self.db)
+            self.record_repository.create_record(user_id = user.id,
+                                       event_id = updated_event.id,
+                                       action = "Update Event")
         return updated_event
