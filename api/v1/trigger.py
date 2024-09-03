e7243fe79af4f9d69058be9219912948157ee0d9
robYP <robchen12@hotmail.com>
2024-09-03 22:22:55 +0800

    get the content based on event_id and user_id

--- api/v1/trigger.py
+++ api/v1/trigger.py
@@ -35,4 +35,15 @@ def get_event_subscribers(event_id: int,
     if not subscribers:
         raise HTTPException(status_code=404, detail="event or subscribers not found")
     return subscribers
+
+
+@router.get("/{event_id}/{user_id}/content")
+def get_event_content(event_id: int,
+                      user_id: int,
+                      user: Annotated[dict, Depends(get_current_user)],
+                      service: TriggerService = Depends(get_trigger_service)):
     
+    content = service.get_event_content(event_id=event_id, user_id=user_id)
+    if not content:
+        raise HTTPException(status_code=404, detail="event or user not found or user not subscribed!")
+    return content
--- services/trigger_service.py
+++ services/trigger_service.py
@@ -1,7 +1,9 @@
 from sqlalchemy.orm import Session
 from repository.event import EventRepository
 from repository.event_user import EventUserRepository
-from typing import List, Dict
+from repository.content import ContentRepository
+from repository.user import UserRepository
+from typing import Optional
 
 
 class TriggerService:
@@ -9,6 +11,8 @@ class TriggerService:
         self.db = db
         self.event_repository = EventRepository(db)
         self.event_user_repository = EventUserRepository(db)
+        self.content_repository = ContentRepository(db)
+        self.user_repository = UserRepository(db)
 
 
     def get_event_route(self, event_id: int):
@@ -26,3 +30,16 @@ class TriggerService:
                 users.append(sub.user_id)
             return users
         return None
+    
+    
+    def get_event_content(self, event_id: int, user_id: int) -> Optional[str]:
+        user = self.user_repository.get_user_by_user_id(user_id)
+        if not user:
+            return None
+
+        subscription = self.event_user_repository.get_subscription(event_id=event_id, user_id=user.id)
+        if not subscription:
+            return None
+
+        content = self.content_repository.get_content(event_id=event_id, language=user.language)
+        return content.content if content else None
