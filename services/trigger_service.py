663e8cba7e13037a8ea1be49b945214f24a3c188
robYP <robchen12@hotmail.com>
2024-09-04 16:46:20 +0800

    added "send notification via email feature": added send_email_notification and process_event in trigger service

--- services/trigger_service.py
+++ services/trigger_service.py
@@ -4,6 +4,10 @@ from repository.event_user import EventUserRepository
 from repository.content import ContentRepository
 from repository.user import UserRepository
 from typing import Optional, List, Dict
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from config.config import GMAIL_SMTP_SERVER, GMAIL_TLS_PORT, GMAIL_SMTP_USERNAME, GMAIL_SMTP_PASSWORD
 
 
 class TriggerService:
@@ -64,3 +68,51 @@ class TriggerService:
             })
         
         return notification_data
+    
+    
+    def send_email_notification(self, event_id: int):
+        event = self.event_repository.search_event_by_id(event_id)
+        if not event:
+            return None
+        
+        route = self.get_event_route(event_id)
+        if route != "EMAIL":
+            return None
+        
+        notification_data = self.get_event_notification_data(event_id)
+        if not notification_data:
+            return None
+        
+        with smtplib.SMTP(GMAIL_SMTP_SERVER) as connection:
+            connection.starttls()
+            connection.login(user=GMAIL_SMTP_USERNAME, password=GMAIL_SMTP_PASSWORD)
+            
+            for subscriber in notification_data["subscribers"]:
+                user = self.user_repository.get_user_by_user_id(subscriber["user_id"])
+                if not user:
+                    continue
+                
+                msg = MIMEMultipart()
+                msg["From"] = GMAIL_SMTP_USERNAME
+                msg["To"] = user.username
+                msg["Subject"] = f"Notification for Event {event.name}, {event_id}"
+                
+                body = subscriber["content"]
+                msg.attach(MIMEText(body, "plain"))
+                
+                connection.send_message(msg)
+                print(f"Email sent to {user.username}")
+            
+        return
+    
+    
+    def process_event(self, event_id: int):
+        route = self.get_event_route(event_id)
+        if not route:
+            return None
+        
+        if route == "EMAIL":
+            self.send_email_notification(event_id)
+            return "email sent"
+        
+        return None
