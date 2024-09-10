50d3d916d2f9fad2aecef731be9351cea982a122
robYP <robchen12@hotmail.com>
2024-09-10 15:23:29 +0800

    get line_user_profile + creatred new line_login_service

--- api/v1/user.py
+++ api/v1/user.py
@@ -11,6 +11,7 @@ from schema.database.user import UserCreate
 from schema.database.token import Token
 import jwt
 from services.auth import AuthService, ACCESS_TOKEN_EXPIRE_MINUTES, get_current_user
+from services.line_login_service import LineLoginService
 from config.config import LINE_CHANNEL_ID, LINE_CALLBACK_URL, LINE_CHANNEL_SECRET
 import requests
 from requests.exceptions import RequestException
@@ -28,6 +29,10 @@ def get_user_service(db: Session = Depends(get_db)) -> UserService:
 
 def get_auth_service(db: Session = Depends(get_db)) -> AuthService:
     return AuthService(db=db)
+
+
+def get_line_login_service(db: Session = Depends(get_db)) -> LineLoginService:
+    return LineLoginService(db=db)
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
@@ -74,55 +79,29 @@ async def read_current_user(user: Annotated[dict, Depends(get_current_user)]):
 
 
 @router.get("/line-login")
-async def line_login():
+async def line_login(service: LineLoginService = Depends(get_line_login_service)):
     line_login_url = f"https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id={LINE_CHANNEL_ID}&redirect_uri={LINE_CALLBACK_URL}&state=12345abcde&scope=profile%20openid"
     return {"LINE login URL": line_login_url}
 
 
-def get_line_access_token(code):
-    token_url = "https://api.line.me/oauth2/v2.1/token"
-    headers = {
-        "Content-Type": "application/x-www-form-urlencoded",
-    }
-    data = {
-        "grant_type": "authorization_code",
-        "code": code,
-        "redirect_uri": LINE_CALLBACK_URL,
-        "client_id": LINE_CHANNEL_ID,
-        "client_secret": LINE_CHANNEL_SECRET
-    }
-    response = requests.post(token_url, headers=headers, data=data)
-    if response.status_code == 200:
-        return response.json()
-    else:
-        raise Exception("Failed to get access token")
-    
-    
-def verify_token(access_token):
-        verify_token_url = "https://api.line.me/oauth2/v2.1/verify"
-        param = {
-            "access_token":access_token,
-        }
-        response = requests.get(verify_token_url, params=param)
-        if response.status_code == 200:
-            return response.json()
-        return None
-    
-
 @router.get("/callback")
-async def line_callback(code: str, state: str):
+async def line_callback(code: str, state: str, service: LineLoginService = Depends(get_line_login_service)):
     # Exchange code for access token
-    print("#######################")
-    print(f"code:{code}")
-    print(f"state:{state}")
-    token_response = get_line_access_token(code)
+    token_response = service.get_line_access_token(code)
     if not token_response:
         raise HTTPException(status_code=400, detail="Failed to get LINE access token")
     print(f"token_response:{token_response}")
    
-    token_info = verify_token(token_response["access_token"])
+    token_info = service.verify_token(token_response["access_token"])
     if not token_info:
         raise HTTPException(status_code=400, detail="Failed to get LINE access token")
     print(f"token_info:{token_info}")
     
-    return token_info
+    user_profile = service.get_line_user_profile(token_response["access_token"])
+    if not user_profile:
+        raise HTTPException(status_code=400, detail="Failed to get LINE user profile")
+    print(f"user_profile:{user_profile}")
+
+    return user_profile
+
+    
