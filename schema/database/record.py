1beb05221ddaf53aca1a952954ef9985f827d0ac
robYP <robchen12@hotmail.com>
2024-10-07 11:12:19 +0800

    added a new column,"created_at", for record table

--- schema/database/record.py
+++ schema/database/record.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
+from datetime import datetime
 
 
 class RecordBase(BaseModel):
@@ -13,6 +14,7 @@ class Record(RecordBase):
     id: int
     user_id: int
     event_id: int
+    created_at: datetime
 
     class Config:
         orm_mode = True
